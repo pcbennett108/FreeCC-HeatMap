b4fa44cb9ad946500c56c9f588b76f09c3e6b484
pcbennett108 <pcbennett42@gmail.com>
2022-10-20 11:49:45 -0600

    Create project skeleton

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log(d3);
+let url =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+let req = new XMLHttpRequest();
+
+let baseTemp;
+let values = [];
+
+let xScale;
+let yScale;
+
+let width = 1200;
+let height = 600;
+let padding = 60;
+
+let canvas = d3.select("#canvas");
+canvas.attr("width", width);
+canvas.attr("height", height);
+
+let generateScales = () => {};
+
+let drawCells = () => {};
+
+let drawAxes = () => {};
+
+req.open("GET", url, true);
+req.onload = () => {
+  let data = JSON.parse(req.responseText);
+  baseTemp = data["baseTemperature"];
+  values = data["monthlyVariance"];
+  console.log(baseTemp, values);
+};
+
+req.send();
